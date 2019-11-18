b27720e6ad2fe125ecfb1a39bd3862f249e1def6
mzooh1112 <mzuhui001@gmail.com>
2019-11-18 19:04:05 +0900

    python file upload

---
+++ upNdown.py
@@ -0,0 +1,36 @@
+# up & down 게임 만들기
+import random # 난수 발생기 
+
+# 임의의 자연수를 반환하는 함수
+# 게임이 시작되면 가장 먼저 실행되는 함수이다.
+def randomNum():
+    pass
+    
+# 사용자의 입력값과 정답이 일치한지 확인하는 함수이다.
+# 일치하면 True 리턴 / 일치하지 않으면 False를 리턴한다.
+def check(answer, user):
+    pass
+
+if __name__=="__main__":
+    # 게임할 사용자의 이름을 입력받는 코드
+    # input 함수를 이용한다.
+    myName = 
+    print("hello, "+myName) 
+
+    # 정답 숫자를 answer변수에 할당한다.
+    answer = randomNum()
+    
+    
+    # while
+    # 사용자가 정답을 맞출 때까지 입력받는다.
+    # 정답이면 정답을 오답이면 오답을 프린트하여 알려준다.
+    while True:
+        # user 변수에 사용자가 입력한 값을 입력받는다.
+        user=
+
+        if ...
+
+        
+        
+        
+    
